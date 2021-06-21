35881606ab9fc29fd44c27c8192a60a328df14df
imononoke <3083545313@qq.com>
2021-06-21 14:34:18 +0800

    do lc problems in Kotlin

---
+++ src/main/kotlin/lc/_1_TwoSum.kt
@@ -0,0 +1,23 @@
+package lc
+
+import java.util.HashMap
+
+// https://leetcode-cn.com/problems/two-sum/
+object _1_TwoSum {
+    fun twoSum(nums: IntArray?, target: Int): IntArray? {
+        if (null == nums) {
+            return null
+        }
+        val res = IntArray(2)
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            if (map.containsKey(nums[i])) {
+                res[0] = map[nums[i]]!!
+                res[1] = i
+                return res
+            }
+            map[target - nums[i]] = i
+        }
+        return res
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
+import lc._1_TwoSum
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    // lc1
+    var arr: IntArray = intArrayOf(3, 5, 9)
+    var lc1 = _1_TwoSum.twoSum(arr, 8)
+    println(lc1?.toList())
 }
